ebc3bc0481002b9bf1625d7054b00d0f23d6f209
Oblv49 <noprivacyworld@gmail.com>
2023-09-18 15:03:50 +0200

    add street to detai/error

--- src/main/java/org/java/lessons/lamiapizzeriacrud/controller/IndexController.java
+++ src/main/java/org/java/lessons/lamiapizzeriacrud/controller/IndexController.java
@@ -3,12 +3,16 @@ package org.java.lessons.lamiapizzeriacrud.controller;
 import org.java.lessons.lamiapizzeriacrud.model.Pizza;
 import org.java.lessons.lamiapizzeriacrud.repository.PizzeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -93,5 +97,19 @@ public class IndexController {
         }
 
         return "/Pizze/List";
+
+    }
+
+    @GetMapping("/show/{pizzaId}")
+    public String show(@PathVariable("pizzaId") Integer id, Model model) {
+        Optional<Pizza> pizzaOptional = pizzaRepository.findById(id);
+        if (pizzaOptional.isPresent()) {
+            Pizza pizzaFromDB = pizzaOptional.get();
+            model.addAttribute("pizza", pizzaFromDB);
+            return "/Pizze/detail";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
     }
 }

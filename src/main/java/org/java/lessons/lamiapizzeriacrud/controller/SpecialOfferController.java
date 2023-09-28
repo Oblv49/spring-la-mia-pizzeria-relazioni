7e7473852b9a9ba200d34a7d2d53937d4dba0031
Oblv49 <noprivacyworld@gmail.com>
2023-09-28 20:23:22 +0200

    add list offer page whit button to delete and edit

--- src/main/java/org/java/lessons/lamiapizzeriacrud/controller/SpecialOfferController.java
+++ src/main/java/org/java/lessons/lamiapizzeriacrud/controller/SpecialOfferController.java
@@ -6,15 +6,15 @@ import org.java.lessons.lamiapizzeriacrud.model.SpecialOffer;
 import org.java.lessons.lamiapizzeriacrud.repository.PizzeRepository;
 import org.java.lessons.lamiapizzeriacrud.repository.SpecialOfferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class SpecialOfferController {
@@ -58,4 +58,66 @@ public class SpecialOfferController {
         return "/specialOffer/offerList";
     }
 
+    @GetMapping("/edit-offer/{id}")
+    public String editSpecialOffer(@PathVariable("id") Integer id, Model model) {
+        Optional<SpecialOffer> specialOfferOptional = specialOfferRepository.findById(id);
+        if (specialOfferOptional.isPresent()) {
+            SpecialOffer specialOffer = specialOfferOptional.get();
+            model.addAttribute("specialOffer", specialOffer); // Assicurati di aggiungere l'offerta speciale al model
+            List<Pizza> pizze = pizzeRepository.findAll();
+            model.addAttribute("pizze", pizze);
+            return "specialOffer/editOfferForm";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/edit-offer/{id}")
+    public String doEditSpecialOffer(@PathVariable("id") Integer id,
+                                     @Valid @ModelAttribute("specialOffer") SpecialOffer specialOfferForm,
+                                     BindingResult bindingResult,
+                                     @RequestParam("pizzeId") List<Integer> pizzeId) {
+        if (bindingResult.hasErrors()) {
+            return "specialOffer/editOfferForm";
+        }
+        Optional<SpecialOffer> specialOfferOptional = specialOfferRepository.findById(id);
+        if (specialOfferOptional.isPresent()) {
+            SpecialOffer specialOffer = specialOfferOptional.get();
+            specialOffer.setStartDate(specialOfferForm.getStartDate());
+            specialOffer.setFinishDate(specialOfferForm.getFinishDate());
+            specialOffer.setOfferTitle(specialOfferForm.getOfferTitle());
+
+            List<Pizza> pizzeList = pizzeRepository.findAllById(pizzeId);
+            specialOfferRepository.save(specialOffer);
+
+            for (Pizza pizza : specialOffer.getPizze()) {
+                pizza.setSpecialOffer(null);
+                pizzeRepository.save(pizza);
+            }
+            for (Pizza pizza : pizzeList) {
+                pizza.setSpecialOffer(specialOffer);
+                pizzeRepository.save(pizza);
+            }
+
+            return "redirect:/offer-list";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/delete-offer/{id}")
+    public String deleteSpecialOffer(@PathVariable("id") Integer id) {
+        Optional<SpecialOffer> specialOfferOptional = specialOfferRepository.findById(id);
+        if (specialOfferOptional.isPresent()) {
+            SpecialOffer specialOffer = specialOfferOptional.get();
+            for (Pizza pizza : specialOffer.getPizze()) {
+                pizza.setSpecialOffer(null);
+                pizzeRepository.save(pizza);
+            }
+            specialOfferRepository.delete(specialOffer);
+            return "redirect:/offer-list";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
 }
